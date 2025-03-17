998983c969e58f9ede9cb3b0e8fc5cb935aee403
João Vitor Assis <JoaoVitorAssis@proton.me>
2025-03-17 17:22:22 -0400

    Adicionando exercicios utilizando while

---
+++ EstruturaWhile.js
@@ -0,0 +1,24 @@
+//Exercicio 1
+var numero = parseInt(prompt("Insira um número inteiro e positivo"))
+var contador = 0
+
+while(contador <= numero){
+    console.log(contador);
+    contador++
+}
+
+//Exercicio 2
+var contador = 0
+
+while(contador <= 50){
+    console.log(contador);
+    contador += 5
+}
+
+//Exercicio 3
+var contador = 50
+
+while(contador >= 0){
+    console.log(contador);
+    contador -= 5
+}
